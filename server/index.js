70ddaf64e2c2c7ca237b2a0031a296705a162ded
Matías <Villalvama@gmail.com>
2017-10-27 12:31:09 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,5 +38,5 @@ app.get('/', function(request, response) {
 });
 
 // listen (start app with node server.js) ======================================
-app.listen(5000);
-console.log("App listening on port 5000");
+app.listen(process.env.PORT || 5000)
+console.log("App listening on port" + process.env.PORT || 5000);
