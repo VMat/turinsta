607abe2b060a063329e49b7bdeb8c3481660376e
Matías <Villalvama@gmail.com>
2017-10-27 16:36:21 -0300

    Update routeServer.js

--- server/router/routeServer.js
+++ server/router/routeServer.js
@@ -1,11 +1,11 @@
 const routeServer = require('express').Router();
-const productApi = require('../rest/productApi');
+const publicationApi = require('../rest/publicationApi');
 
 routeServer.get('/', function (req, res){
   res.json(200, {msg: 'OK' });
 });
 
 // API REST PRODUCTOS
-routeServer.use('/productos', productApi);
+routeServer.use('/productos', publicationApi);
 
 module.exports = routeServer;
