fc9d7054db5c3abd19030f0626d48469a47d1b9e
Matías <Villalvama@gmail.com>
2017-10-30 16:57:00 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(cors());
 
 app.use((req, res, next)=>{
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header('Access-Control-Allow-Methods', 'DELETE, GET');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
