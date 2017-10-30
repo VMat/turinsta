e1fc0ce0459d2886585fc7eb7594b66984bc937f
Matías <Villalvama@gmail.com>
2017-10-30 14:59:12 -0300

    Update comment.js

--- server/models/comment.js
+++ server/models/comment.js
@@ -12,6 +12,6 @@ const commentSchema = new mongoose.Schema({
 	]
 });
 
-Comments = mongoose.model('Comments', commentSchema, 'Comments');
+const Comments = mongoose.model('Comments', commentSchema, 'Comments');
 
 module.exports = Comments;
