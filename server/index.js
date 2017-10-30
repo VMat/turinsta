52f9107709674ba2251f3ac0dcb5c930561b59a7
Matías <Villalvama@gmail.com>
2017-10-30 17:02:55 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use((req, res, next)=>{
 
 // Routes
 app.use('/api', routeServer);
-//app.use(express.static(path.join(__dirname + '/../www')));
+app.use(express.static(path.join(__dirname + '/../www')));
 app.use('/static', express.static(path.join(__dirname + '/assets')));
 
 // views is directory for all template files
