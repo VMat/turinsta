2040e097014126f5275a2c506859407941658adc
Matías <Villalvama@gmail.com>
2017-10-30 14:58:15 -0300

    Update comment.js

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,1 +1,17 @@
+const mongoose = require('mongoose');
 
+// Mongoose commentSchema definition
+const commentSchema = new mongoose.Schema({
+	user: {
+		type: Schema.Types.ObjectId, ref: 'Users'
+	},
+	date: String,
+	content: String,
+	replies: [
+    {type: Schema.Types.ObjectId, ref: 'Comments'}
+	]
+});
+
+Comments = mongoose.model('Comments', commentSchema, 'Comments');
+
+module.exports = Comments;
