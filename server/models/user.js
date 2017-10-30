34b35155dd4637066d775c561e962989989c22d9
Matías <Villalvama@gmail.com>
2017-10-30 15:06:24 -0300

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 // Mongoose userSchema definition
 const userSchema = new mongoose.Schema({
-	"avatar": String,
-	"username": String,
-	"score": Number,
-	"publicationIds": []
+	avatar: String,
+	username: String,
+	score: Number,
+	publicationIds: []
 });
 
-Users = mongoose.model('Users', userSchema, 'Users');
+const Users = mongoose.model('Users', userSchema, 'Users');
 
 module.exports = Users;
