dcf7f7a4f98ba92fbee3021601c4350e2f32dbf7
Matías <Villalvama@gmail.com>
2017-11-02 15:07:44 -0300

    Create language.js

---
+++ server/models/language.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Mongoose languageSchema definition
+const languageSchema = new Schema({
+	name: String,
+	short: String
+});
+
+const Languages = mongoose.model('Languages', languageSchema, 'Languages');
+
+module.exports = Languages;
