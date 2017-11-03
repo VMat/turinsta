d566509f557a048ca583b608729e07862f2f5df8
Matías <Villalvama@gmail.com>
2017-11-03 16:24:15 -0300

    Create userInterface.js

---
+++ server/db/userInterface.js
@@ -0,0 +1,39 @@
+const Users = require('../models/user');
+const Publications = require('../models/publication');
+const Commons = require('./commons');
+
+const UserInterface = (function(){
+
+  function oUserInterface(){}
+
+  oUserInterface.prototype = {
+
+    getAll: ()=>{
+      return Commons.getAll(Users);
+    },
+
+    getOne: (id)=>{
+      return Commons.getOne(Users, id)        
+        .populate('publications');
+    },
+    
+    insert: (user)=>{
+      return Commons.insert(new Users(user));
+    },
+
+    update: (user)=>{
+      return Commons.update(Users, user);
+    },
+
+    deleteOne: (id)=>{
+      return Commons.removeOne(Users, id);
+    }
+  }
+
+  return oUserInterface;
+
+})();
+
+oUserInterface = new UserInterface();
+
+module.exports = oUserInterface;
