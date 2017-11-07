2a075c47025502b8574dabf11bc37cab50eb2d27
Matías <Villalvama@gmail.com>
2017-11-07 15:30:15 -0300

    Update publication.service.ts

--- src/providers/storage/publication.service.ts
+++ src/providers/storage/publication.service.ts
@@ -9,12 +9,21 @@ export class PublicationService {
     console.log('Hello StorageProvider Provider');
   }
 
-  getPublications() {
-    
-    //return this.storageService.getPublications();
-    return Observable.timer(1000).mapTo(this.storageService.getPublications());
+  getPublications() {  
+    return this.storageService.getPublications();
+    //return Observable.timer(1000).mapTo(this.storageService.getPublications());
+    //turn Observable.timer(5000).first().subscribe(() => this.refreshData());
   }
 
-
+  refreshData(){
+    return this.storageService.getPublications().subscribe(publications => {
+        this.publications = publications;
+        return this.subscribeToData();
+    });
+  }
+  
+  private subscribeToData(){
+    return Observable.timer(5000).first().subscribe(() => this.refreshData());
+  }
 
 }
