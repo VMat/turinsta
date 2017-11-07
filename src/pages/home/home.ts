7b33a1e25f3372852094ca3fa9372e2dc91134a2
Matías <Villalvama@gmail.com>
2017-11-07 16:13:49 -0300

    Update home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-// import {StorageProvider} from "../../providers/storage/storage";
+import {StorageProvider} from "../../providers/storage/storage";
 import {Observable} from "rxjs";
 import {Store} from "@ngrx/store";
 import {getPublications} from "../../providers/reducers/publication.reducer";
+import {AnonymousSubscription} from "rxjs/Subscription";
 
 interface AppState {
   publication: any
@@ -13,13 +14,16 @@ interface AppState {
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy{
 
-  publications : Observable<any>;
+  //publications : Observable<any>;
+  private timerSubscription: AnonymousSubscription;
+  private publicationSubscription: AnonymousSubscription;
+  private publications: Any;
 
-  constructor(public navCtrl: NavController, private store: Store<AppState>) {
-    this.store.dispatch(getPublications());
-    this.publications = store.select("publication");
+  constructor(public storageService:StorageProvider, public navCtrl: NavController, private store: Store<AppState>) {
+    //this.store.dispatch(getPublications());
+    //this.publications = store.select("publication");
   }
 
   // getPublications(){
@@ -28,4 +32,29 @@ export class HomePage {
   //     this.publications = data;
   //   });
   // }
+  
+    public ngOnInit(): void {
+        this.refreshData();
+    }
+
+    public ngOnDestroy(): void {
+        if (this.postsSubscription) {
+            this.postsSubscription.unsubscribe();
+        }
+        if (this.timerSubscription) {
+            this.timerSubscription.unsubscribe();
+        }
+    }
+  
+    private refreshData(): void {
+        this.postsSubscription = this.storageService.getPublications().subscribe(publications => {
+            this.publications = publications;
+            this.subscribeToData();
+        });
+    }
+
+    private subscribeToData(): void {
+        this.timerSubscription = Observable.timer(5000).first().subscribe(() => this.refreshData());
+    }
+  
 }
