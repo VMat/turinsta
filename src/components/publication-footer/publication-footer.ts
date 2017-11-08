0da3b966de7c694f705f7aecd038ab11e4c474ef
Matías <Villalvama@gmail.com>
2017-11-08 14:49:41 -0300

    Update publication-footer.ts

--- src/components/publication-footer/publication-footer.ts
+++ src/components/publication-footer/publication-footer.ts
@@ -13,7 +13,7 @@ import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 })
 export class PublicationFooterComponent {
 
-  //@Input() data: any = null;
+  /*@Input() data: any = null;*/
   data: any = {comments:[],experiences:[]};
   sections: any = [{name: "Experiences", show: false}, {name: "Comments", show: false}];
 
