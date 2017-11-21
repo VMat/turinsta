33c23aef00a68a217518a8ba50bbe45e9375f3a5
VMat <villalvama@gmail.com>
2017-11-20 23:00:58 -0300

    Test create, update && delete comment

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use((req, res, next)=>{
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  // res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
