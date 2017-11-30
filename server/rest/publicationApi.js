639fa87aa283830ba6b198b62f2a38af07499448
Matías <Villalvama@gmail.com>
2017-11-30 14:53:10 -0300

    Update publicationApi.js

--- server/rest/publicationApi.js
+++ server/rest/publicationApi.js
@@ -4,7 +4,7 @@ const publicationService = require('../services/publicationService');
 router.get('/count/:count/sort/:field/:way',(req, res)=>{
   console.log("c: " + req.params.count);
   let searchParams = JSON.parse(decodeURI(JSON.stringify(req.query)))
-  let orderBy = {...{[req.params.field]: req.params.way}, ...{"timestamps.created": -1}};
+  let orderBy = {...{[req.params.field]: req.params.way}, ...{"timestamps.created": 1}};
   publicationService.getPublications(searchParams,req.params.count,orderBy)
     .then(publications=>{res.status(200).json(publications)})
     .catch(error=>{res.status(500).send(error)})
