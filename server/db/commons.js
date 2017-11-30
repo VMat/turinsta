2ea7cd02aad86bd1a49124b87d0ccaf3af3cb6e5
Matías <Villalvama@gmail.com>
2017-11-30 17:14:20 -0300

    Update commons.js

--- server/db/commons.js
+++ server/db/commons.js
@@ -32,6 +32,43 @@ let Commons = (function(){
 
     removeOne: (Collection,doc)=>{
       return Collection.findByIdAndRemove(doc._id);
+    },
+    
+    processParams: (params) => {
+      let filters = {};
+      
+      for(let i in params){
+        
+        switch(params[i].operation){
+      
+            case 'EQUAL':{
+              filters[i] = params[i].value;
+              break;
+            }
+            case 'LIKE':{
+              filters[i] = {"$regex": params[i].value, "$options": "i"}
+              break;
+            }
+            case 'HIGHER_THAN':{
+              filters[i] = { $gt: params[i].value };
+              break;
+            } 
+            case 'LOWER_THAN':{
+              filters[i] = { $lt: params[i].value };
+              break;
+            }  
+            case 'BETWEEN':{
+              filters[i] = { $gt: params[i].value[0], $lt: params[i].value[1] };
+              break;
+            }  
+            case 'IN':{
+              filters[i] = {$in: params[i].value};
+              break;
+            }          
+        }
+      }
+     
+      return filters;
     }
 
   };
