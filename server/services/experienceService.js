de8602b8bb6d8e8f3ce456efa756a7d953e808f9
Matías <Villalvama@gmail.com>
2017-12-05 16:47:59 -0300

    Update experienceService.js

--- server/services/experienceService.js
+++ server/services/experienceService.js
@@ -1,1 +1,48 @@
+const storageService = require('./storageService');
 
+const experienceService = (function(){
+
+  function oExperienceService(){}
+
+  oExperienceService.prototype = {
+
+    getExperience: (id)=>{
+      return new Promise((resolve, reject)=>{
+        storageService.getExperience(id).
+          then(experiences=>resolve(experiences)).
+          catch(error=>reject(error))
+      })
+    },
+
+    createExperience: (experience)=>{
+      return new Promise((resolve, reject)=>{
+        storageService.createExperience(experience).
+          then(newExperience=>resolve(newExperience)).
+          catch(error=>reject(error))
+      })
+    },
+
+    updateExperience: (experience)=>{
+      return new Promise((resolve, reject)=>{
+        storageService.updateExperience(experience).
+          then(updatedExperience=>resolve(updatedExperience)).
+          catch(error=>reject(error))
+      })
+    },
+
+    deleteExperience: (id)=>{
+      return new Promise((resolve, reject)=>{
+      storageService.deleteExperience(id).
+        then(deletedExperience=>resolve(deletedExperience)).
+        catch(error=>reject(error))
+      })
+    }
+  };
+
+  return oExperienceService;
+
+})();
+
+const oExperienceService = new experienceService();
+
+module.exports = oExperienceService;
