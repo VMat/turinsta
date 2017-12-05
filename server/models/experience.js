c461ecf168e6a6a0ba375afaf5b8c4ff3428b65c
Matías <Villalvama@gmail.com>
2017-12-05 15:05:35 -0300

    Update experience.js

--- server/models/experience.js
+++ server/models/experience.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 // Mongoose experienceSchema definition
 const experienceSchema = new mongoose.Schema({
+  publication: {type: schema.Types.ObjectId, ref: 'Publications'},
 	category: String,
 	content: String,
 	score: Number	
