3ba980075ce5d11336e5c529dba0f75e1eed859d
Matías <Villalvama@gmail.com>
2017-12-06 15:26:03 -0300

    Update userApi.js

--- server/rest/userApi.js
+++ server/rest/userApi.js
@@ -25,4 +25,12 @@ router.put('/',(req, res)=>{
     .catch(error=>{res.status(500).send(error)})
 });
 
+router.post('/favorites',(req, res)=>{
+  userService.addFavoritePublication(req.body)
+    .then(user=>{res.status(200).json(user)})
+    .catch(error=>{res.status(500).send(error)})
+});
+
+
+
 module.exports = router;
