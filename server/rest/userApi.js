a77dd1a181fd8ea56e14a15f56340424683f0aea
Matías <Villalvama@gmail.com>
2017-12-07 17:25:04 -0300

    Update userApi.js

--- server/rest/userApi.js
+++ server/rest/userApi.js
@@ -37,5 +37,17 @@ router.delete('/favorites',(req, res)=>{
     .catch(error=>{res.status(500).send(error)})
 });
 
+router.post('/followers',(req, res)=>{
+  userService.addUserFollower(req.body)
+    .then(user=>{res.status(200).json(user)})
+    .catch(error=>{res.status(500).send(error)})
+});
+
+router.delete('/followers',(req, res)=>{
+  userService.removeUserFollower(req.body)
+    .then(user=>{res.status(200).json(user)})
+    .catch(error=>{res.status(500).send(error)})
+});
+
 
 module.exports = router;
