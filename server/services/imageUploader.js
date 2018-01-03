99b860aa39a20c516290a74de69e53b15ddae490
Matías <Villalvama@gmail.com>
2018-01-03 15:47:19 -0300

    Create imageUploader.js

---
+++ server/services/imageUploader.js
@@ -0,0 +1,14 @@
+
+const ImageUploaderService = (function(){
+
+  function oImageUploaderService(){}
+
+  oImageUploaderService.prototype = {};
+  
+  return oImageUploaderService;
+  
+})();
+
+const oImageUploaderService = new ImageUploaderService();
+
+module.export = oImageUploaderService;
