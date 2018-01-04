fe004d679f6e89441f6ea32c5c100fefb860120c
Matías <Villalvama@gmail.com>
2018-01-04 15:16:19 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,8 +15,8 @@ const routeServer = require('./router/routeServer');
 // Configuration
 storageService.connect();
 
-/*app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(morgan('dev'));                                         // log every request to the console
+/*app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
