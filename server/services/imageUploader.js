49a9c8def92f75944b19979126c2108010b869a6
Matías <Villalvama@gmail.com>
2018-01-05 13:56:43 -0300

    Update imageUploader.js

--- server/services/imageUploader.js
+++ server/services/imageUploader.js
@@ -19,6 +19,9 @@ let ImgUpload = {};
 ImgUpload.uploadToGcs = (req, res, next) => {
   console.log("uploadToGcs");
   //console.log("Req: " + JSON.stringify(req));
+  console.log("file: " + req.file);
+  console.log("files: " + req.files);
+  console.log("turinstafile: " + req.turinstafile);
   
   if(!req.file) return next();
 
