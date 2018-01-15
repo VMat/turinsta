96483e8b983805fbce19eec49ddc87f0e8e7918a
Matías <Villalvama@gmail.com>
2018-01-15 16:05:50 -0300

    Update caption.js

--- server/models/caption.js
+++ server/models/caption.js
@@ -3,12 +3,41 @@ const Schema = mongoose.Schema;
 
 // Mongoose captionSchema definition
 const captionSchema = new Schema({
-  message: [{
-		language: {
-			type: Schema.Types.ObjectId, ref: 'Languages'
-		},
-		transcription: String
-	}]
+  "language": {
+    "name": String,
+    "short": String,
+    "flag": String
+  },
+  "glosary": {
+        "timeUnits": {
+            "YEAR": {
+                "SINGULAR": String,
+                "PLURAL": String
+            },
+            "MONTH": {
+                "SINGULAR": String,
+                "PLURAL": String
+            },
+            "DAY": {
+                "SINGULAR": String,
+                "PLURAL": String
+            },
+            "HOUR": {
+                "SINGULAR": String,
+                "PLURAL": String
+            },
+            "MINUTE": {
+                "SINGULAR": String,
+                "PLURAL": String
+            },
+            "SECOND": {
+                "SINGULAR": String,
+                "PLURAL": String
+            }
+        },
+        "antiquitySentence": String,
+        "veryRecentActivitySentence": String
+    }
 });
 
 const Captions = mongoose.model('Captions', captionSchema, 'Captions');
