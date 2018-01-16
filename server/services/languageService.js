197e59042180bd24fe70ca0dae8997da38c70650
Matías <Villalvama@gmail.com>
2018-01-16 15:51:46 -0300

    Update languageService.js

--- server/services/languageService.js
+++ server/services/languageService.js
@@ -1,1 +1,21 @@
+const storageService = require('./storageService');
 
+let languageService = {};
+  
+languageService.getLanguages = ()=>{      
+  return new Promise((resolve, reject)=>{
+    storageService.getLanguages().
+      then(languages=>resolve(languages)).
+      catch(error=>reject(error))        
+  })
+};
+    
+languageService.getLanguage = (id)=>{
+  return new Promise((resolve, reject)=>{
+    storageService.getLanguage(id).
+      then(language=>resolve(language)).
+      catch(error=>reject(error))        
+  })                
+};
+    
+module.exports = languageService;
