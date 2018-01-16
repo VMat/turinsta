2a7533ae4eadf3b2dd903bc31bf3c78847c432b5
Matías <Villalvama@gmail.com>
2018-01-16 15:01:42 -0300

    Update commentInterface.js

--- server/db/commentInterface.js
+++ server/db/commentInterface.js
@@ -5,111 +5,99 @@ const Commons = require('./commons');
 const UserInterface = require('./userInterface');
 const PublicationInterface = require('./publicationInterface');
 
-const CommentInterface = (function(){
+let CommentInterface = {};
 
-  function oCommentInterface(){}
+CommentInterface.getOne = (id)=>{
+  return Commons.getOne(Comments, id);
+};
 
-  oCommentInterface.prototype = {
+CommentInterface.insert = (comment)=>{
+  return UserInterface.getOne(comment.user)
+    .then(user=>{
+      let oComment = {};
+      oComment.user = {
+        _id: user._id,
+        username: user.username,
+        avatar: user.avatar
+      };
+      oComment.publication = comment.publication;
+      oComment.parent = comment.parent;
+      oComment.content = comment.content;
+      return Commons.insert(new Comments(oComment))
+        .then(insertedComment=>{
+          if(comment.parent!=null){
+            return Commons.getOne(Comments,comment.parent)
+            .then(comment=>{
+              comment.replies.push({
+                _id: insertedComment._id,
+                user: insertedComment.user,
+                date: insertedComment.date,
+                content: insertedComment.content
+              });
+              return Commons.update(Comments,comment)
+            })
+          }
+          else{
+            return PublicationInterface.getOne(insertedComment.publication)
+            .then(publication=>{
+              publication.commentIds.push(insertedComment._id);
+              return PublicationInterface.update(publication)
+            })
+          }
+      });
+    });
+};
 
-    getOne: (id)=>{
-      return Commons.getOne(Comments, id);
-    },
+CommentInterface.update = (comment)=>{
+  if(Boolean(comment.parent)){
+    return Commons.getOne(Comments,comment.parent)
+      .then(parent=>{
+        parent.replies.filter((reply)=>{
+          return reply._id.equals(comment._id);
+        })[0].content = comment.content;
+        return Commons.update(Comments,parent)
+          .then(()=>{
+            return Commons.update(Comments, comment);
+          })
+      });
+  }
+  else{
+    return Commons.update(Comments, comment);
+  }
+};
 
-    insert: (comment)=>{
-      return UserInterface.getOne(comment.user)
-        .then(user=>{
-          let oComment = {};
-          oComment.user = {
-            _id: user._id,
-            username: user.username,
-            avatar: user.avatar
-          };
-          oComment.publication = comment.publication;
-          oComment.parent = comment.parent;
-          oComment.content = comment.content;
-          return Commons.insert(new Comments(oComment))
-            .then(insertedComment=>{
-              if(comment.parent!=null){
-                return Commons.getOne(Comments,comment.parent)
-                .then(comment=>{
-                  comment.replies.push({
-                    _id: insertedComment._id,
-                    user: insertedComment.user,
-                    date: insertedComment.date,
-                    content: insertedComment.content
-                  });
-                  return Commons.update(Comments,comment)
-                })
-              }
-              else{
-                return PublicationInterface.getOne(insertedComment.publication)
-                .then(publication=>{
-                  publication.commentIds.push(insertedComment._id);
-                  return PublicationInterface.update(publication)
-                })
-              }
-          });
-        });
-    },
-
-    update: (comment)=>{
-      if(Boolean(comment.parent)){
+CommentInterface.deleteOne = (id)=>{
+  return Commons.getOne(Comments,id)
+    .then(comment=>{
+      if(comment.parent != null){
         return Commons.getOne(Comments,comment.parent)
           .then(parent=>{
-            parent.replies.filter((reply)=>{
-              return reply._id.equals(comment._id);
-            })[0].content = comment.content;
+            parent.replies.filter((reply,i)=>{
+              if(reply._id.equals(comment._id)){
+                parent.replies.splice(i,1);
+              }
+            });
             return Commons.update(Comments,parent)
               .then(()=>{
-                return Commons.update(Comments, comment);
+                return Commons.removeOne(Comments, comment);
               })
           });
       }
       else{
-        return Commons.update(Comments, comment);
-      }
-    },
-
-    deleteOne: (id)=>{
-      return Commons.getOne(Comments,id)
-        .then(comment=>{
-          if(comment.parent != null){
-            return Commons.getOne(Comments,comment.parent)
-              .then(parent=>{
-                parent.replies.filter((reply,i)=>{
-                  if(reply._id.equals(comment._id)){
-                    parent.replies.splice(i,1);
-                  }
-                });
-                return Commons.update(Comments,parent)
-                  .then(()=>{
-                    return Commons.removeOne(Comments, comment);
-                  })
-              });
-          }
-          else{
-            return Commons.getOne(Publications, comment.publication)
-              .then(publication=>{
-                publication.commentIds.splice(publication.commentIds.indexOf(comment._id), 1);
-                return PublicationInterface.update(publication)
-                  .then(()=>{
-                    return Commons.removeOne(Comments, comment);
-                  })
+        return Commons.getOne(Publications, comment.publication)
+          .then(publication=>{
+            publication.commentIds.splice(publication.commentIds.indexOf(comment._id), 1);
+            return PublicationInterface.update(publication)
+              .then(()=>{
+                return Commons.removeOne(Comments, comment);
               })
-          }
-        })
-    },
+          })
+      }
+    })
+};
     
-    deleteFromPublication: (id)=>{
-      return Commons.removeWithFilter(Comments,{"publication": id});
-    }
+CommentInterface.deleteFromPublication = (id)=>{
+  return Commons.removeWithFilter(Comments,{"publication": id});
+};
     
-  };
-
-  return oCommentInterface;
-
-})();
-
-oCommentInterface = new CommentInterface();
-
-module.exports = oCommentInterface;
+module.exports = CommentInterface;
