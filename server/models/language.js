10f30e22ce7a58dc300ce046bb92b8e01fb384e3
Matías <Villalvama@gmail.com>
2018-01-16 18:36:38 -0300

    Update language.js

--- server/models/language.js
+++ server/models/language.js
@@ -6,36 +6,7 @@ const languageSchema = new Schema({
   "name": String,
   "short": String,
   "flag": String,
-  "glosary": {
-        "timeUnits": {
-            "YEAR": {
-                "SINGULAR": String,
-                "PLURAL": String
-            },
-            "MONTH": {
-                "SINGULAR": String,
-                "PLURAL": String
-            },
-            "DAY": {
-                "SINGULAR": String,
-                "PLURAL": String
-            },
-            "HOUR": {
-                "SINGULAR": String,
-                "PLURAL": String
-            },
-            "MINUTE": {
-                "SINGULAR": String,
-                "PLURAL": String
-            },
-            "SECOND": {
-                "SINGULAR": String,
-                "PLURAL": String
-            }
-        },
-        "antiquitySentence": String,
-        "veryRecentActivitySentence": String
-    }
+  "glosary": {}
 });
 
 const Languages = mongoose.model('Languages', languageSchema, 'Languages');
