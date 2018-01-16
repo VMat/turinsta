f2a192689fde86b5e0111e9888f8509833127252
Matías <Villalvama@gmail.com>
2018-01-16 14:51:36 -0300

    Update activityService.js

--- server/services/activityService.js
+++ server/services/activityService.js
@@ -1,56 +1,45 @@
 const storageService = require('./storageService');
 
-const activityService = (function(){
-
-  function oActivityService(){}
+let activityService = {};
   
-  oActivityService.prototype = {
-  
-    getActivities: ()=>{      
-      return new Promise((resolve, reject)=>{
-        storageService.getActivities().
-          then(activities=>resolve(activities)).
-          catch(error=>reject(error))        
-      })
-    },
+activityService.getActivities = ()=>{      
+  return new Promise((resolve, reject)=>{
+    storageService.getActivities().
+      then(activities=>resolve(activities)).
+      catch(error=>reject(error))        
+  })
+};
     
-    getActivity: (id)=>{
-      return new Promise((resolve, reject)=>{
-        storageService.getActivity(id).
-          then(activities=>resolve(activities)).
-          catch(error=>reject(error))        
-      })                
-    },
+activityService.getActivity = (id)=>{
+  return new Promise((resolve, reject)=>{
+    storageService.getActivity(id).
+      then(activities=>resolve(activities)).
+      catch(error=>reject(error))        
+  })                
+};
     
-    createActivity: (newActivity)=>{      
-      return new Promise((resolve, reject)=>{
-        storageService.createActivity(newActivity).
-          then(activity=>resolve(activity)).
-          catch(error=>reject(error))        
-      })
-    },
+activityService.createActivity = (newActivity)=>{      
+  return new Promise((resolve, reject)=>{
+    storageService.createActivity(newActivity).
+      then(activity=>resolve(activity)).
+      catch(error=>reject(error))        
+  })
+};
     
-    updateActivity: (activityToUpdate)=>{
-      return new Promise((resolve, reject)=>{
-        storageService.updateActivity(activityToUpdate).
-          then(activity=>resolve(activity)).
-          catch(error=>reject(error))        
-      })
-    },
+activityService.updateActivity = (activityToUpdate)=>{
+  return new Promise((resolve, reject)=>{
+    storageService.updateActivity(activityToUpdate).
+      then(activity=>resolve(activity)).
+      catch(error=>reject(error))        
+  })
+};
         
-    deleteActivity: (activity)=>{
-      return new Promise((resolve, reject)=>{
-        storageService.deleteActivity(activity).
-          then(activity=>resolve(activity)).
-          catch(error=>reject(error))        
-      })  
-    }
-  };
+activityService.deleteActivity = (activity)=>{
+  return new Promise((resolve, reject)=>{
+    storageService.deleteActivity(activity).
+      then(activity=>resolve(activity)).
+      catch(error=>reject(error))        
+  })  
+};
 
-  return oActivityService;
-
-})();
-
-const oActivityService = new activityService();
-
-module.exports = oActivityService;
+module.exports = activityService;
