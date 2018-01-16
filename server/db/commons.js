648bd3d34ec44127bf05b6cc2008473a01db4893
Matías <Villalvama@gmail.com>
2018-01-16 15:04:44 -0300

    Update commons.js

--- server/db/commons.js
+++ server/db/commons.js
@@ -1,137 +1,123 @@
 const mongoose = require('mongoose');
 
-let Commons = (function(){
+let Commons = {};
 
-  function oCommons(){}
+Commons.getAll = (Collection)=>{
+  return Collection.find()
+};
 
-  oCommons.prototype = {
+Commons.getN = (Collection,filters,n,order)=>{
+  return Collection.find(filters)
+      .sort(order)
+      .limit(Number(n));
+};
 
-    getAll: (Collection)=>{
-      return Collection.find()
-    },
+Commons.getOne = (Collection,id)=>{
+  return Collection.findById(id);
+};
 
-    getN: (Collection,filters,n,order)=>{
-      return Collection.find(filters)
-          .sort(order)
-          .limit(Number(n));
-    },
+Commons.insert = (newPublication)=>{
+  return newPublication.save();
+};
 
-    getOne: (Collection,id)=>{
-      return Collection.findById(id);
-    },
-
-    insert: (newPublication)=>{
-      return newPublication.save();
-    },
-
-    patch: (Collection,id,fields)=>{
-      return Collection.findByIdAndUpdate(id,{$set: fields});
-    },
+Commons.patch = (Collection,id,fields)=>{
+  return Collection.findByIdAndUpdate(id,{$set: fields});
+};
     
-    update: (Collection,doc)=>{
-      return Collection.findByIdAndUpdate(doc._id, doc, {upsert:false});
-    },
+Commons.update = (Collection,doc)=>{
+  return Collection.findByIdAndUpdate(doc._id, doc, {upsert:false});
+};
 
-    removeAll: (Collection)=>{
-      return Collection.remove();
-    },
+Commons.removeAll = (Collection)=>{
+  return Collection.remove();
+};
     
-    removeWithFilter: (Collection,filter)=>{
-      return Collection.remove(filter);
-    },
+Commons.removeWithFilter = (Collection,filter)=>{
+  return Collection.remove(filter);
+};
 
-    removeOne: (Collection,doc)=>{
-      return Collection.findByIdAndRemove(doc._id);
-    },
+Commons.removeOne = (Collection,doc)=>{
+  return Collection.findByIdAndRemove(doc._id);
+};
 
-    processParams: (params) => {
-      let filters = [];
+Commons.processParams = (params) => {
+  let filters = [];
 
-      for(let i in params){
+  for(let i in params){
 
-        switch(params[i].operation){
+    switch(params[i].operation){
 
-          case 'EQUAL':{
-            filters[i] = params[i].value;
-            break;
-          }
-          case 'LIKE':{
-            filters[i] = {"$regex": params[i].value, "$options": "i"};
-            break;
-          }
-          case 'HIGHER_THAN':{
-            filters[i] = { $gt: params[i].value };
-            break;
-          }
-          case 'LOWER_THAN':{
-            filters[i] = { $lt: params[i].value };
-            break;
-          }
-          case 'BETWEEN':{
-            filters[i] = { $gt: params[i].value[0], $lt: params[i].value[1] };
-            break;
-          }
-          case 'IN':{
-            filters[i] = {$in: params[i].value};
-            break;
-          }
-        }
+      case 'EQUAL':{
+        filters[i] = params[i].value;
+        break;
       }
-
-      console.log("filters: " + JSON.stringify(filters));
-
-      return filters;
-    },
-
-    processAggregateParams: (params) => {
-      let filters = [];
-
-      for(let i in params){
-
-        switch(params[i].operation){
-
-            case 'EQUAL':{
-              filters.push({$match: {[i]: params[i].value}});
-              break;
-            }
-            case 'LIKE':{
-              filters.push({$match: {[i]: {$regex: params[i].value, $options: "i"}}});
-              break;
-            }
-            case 'HIGHER_THAN':{
-              filters.push({$match: {[i]: {$gt: params[i].value}}});
-              break;
-            }
-            case 'LOWER_THAN':{
-              filters.push({$match: {[i]: {$lt: params[i].value}}});
-              break;
-            }
-            case 'BETWEEN':{
-              filters.push({$match: {[i]: {$gt: params[i].value[0], $lt: params[i].value[1]}}});
-              break;
-            }
-            case 'IN':{
-              filters.push({$match: {[i]: {$in: params[i].value}}});
-              break;
-            }
-            case 'CONTAINS':{
-              filters.push({$match: {[i]: {$elemMatch: {$eq: params[i].value}}}});
-              break;
-            }
-        }
+      case 'LIKE':{
+        filters[i] = {"$regex": params[i].value, "$options": "i"};
+        break;
       }
-
-      console.log("filters: " + JSON.stringify(filters));
-
-      return filters;
+      case 'HIGHER_THAN':{
+        filters[i] = { $gt: params[i].value };
+        break;
+      }
+      case 'LOWER_THAN':{
+        filters[i] = { $lt: params[i].value };
+        break;
+      }
+      case 'BETWEEN':{
+        filters[i] = { $gt: params[i].value[0], $lt: params[i].value[1] };
+        break;
+      }
+      case 'IN':{
+        filters[i] = {$in: params[i].value};
+        break;
+      }
     }
+  }
 
-  };
+  console.log("filters: " + JSON.stringify(filters));
 
-  return oCommons;
+  return filters;
+};
 
-})();
+Commons.processAggregateParams = (params) => {
+  let filters = [];
 
-const oCommons = new Commons();
+  for(let i in params){
 
-module.exports = oCommons;
+    switch(params[i].operation){
+
+        case 'EQUAL':{
+          filters.push({$match: {[i]: params[i].value}});
+          break;
+        }
+        case 'LIKE':{
+          filters.push({$match: {[i]: {$regex: params[i].value, $options: "i"}}});
+          break;
+        }
+        case 'HIGHER_THAN':{
+          filters.push({$match: {[i]: {$gt: params[i].value}}});
+          break;
+        }
+        case 'LOWER_THAN':{
+          filters.push({$match: {[i]: {$lt: params[i].value}}});
+          break;
+        }
+        case 'BETWEEN':{
+          filters.push({$match: {[i]: {$gt: params[i].value[0], $lt: params[i].value[1]}}});
+          break;
+        }
+        case 'IN':{
+          filters.push({$match: {[i]: {$in: params[i].value}}});
+          break;
+        }
+        case 'CONTAINS':{
+          filters.push({$match: {[i]: {$elemMatch: {$eq: params[i].value}}}});
+          break;
+        }
+    }
+  }
+
+  return filters;
+};
+
+module.exports = Commons;
