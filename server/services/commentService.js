7286b2e7bf76e4f6ac60ae6b2450a63c45e99e6d
Matías <Villalvama@gmail.com>
2018-01-16 14:53:43 -0300

    Update commentService.js

--- server/services/commentService.js
+++ server/services/commentService.js
@@ -1,48 +1,37 @@
 const storageService = require('./storageService');
 
-const commentService = (function(){
+let commentService = {};
 
-  function oCommentService(){}
+commentService.getComment = (id)=>{
+  return new Promise((resolve, reject)=>{
+    storageService.getComment(id).
+      then(comments=>resolve(comments)).
+      catch(error=>reject(error))
+  })
+};
 
-  oCommentService.prototype = {
+commentService.createComment = (comment)=>{
+  return new Promise((resolve, reject)=>{
+    storageService.createComment(comment).
+      then(newComment=>resolve(newComment)).
+      catch(error=>reject(error))
+  })
+};
 
-    getComment: (id)=>{
-      return new Promise((resolve, reject)=>{
-        storageService.getComment(id).
-          then(comments=>resolve(comments)).
-          catch(error=>reject(error))
-      })
-    },
+commentService.updateComment = (comment)=>{
+  return new Promise((resolve, reject)=>{
+    storageService.updateComment(comment).
+      then(updatedComment=>resolve(updatedComment)).
+      catch(error=>reject(error))
+  })
+};
 
-    createComment: (comment)=>{
-      return new Promise((resolve, reject)=>{
-        storageService.createComment(comment).
-          then(newComment=>resolve(newComment)).
-          catch(error=>reject(error))
-      })
-    },
+commentService.deleteComment = (id)=>{
+  return new Promise((resolve, reject)=>{
+  storageService.deleteComment(id).
+    then(deletedComment=>resolve(deletedComment)).
+    catch(error=>reject(error))
+  })
+};
 
-    updateComment: (comment)=>{
-      return new Promise((resolve, reject)=>{
-        storageService.updateComment(comment).
-          then(updatedComment=>resolve(updatedComment)).
-          catch(error=>reject(error))
-      })
-    },
-
-    deleteComment: (id)=>{
-      return new Promise((resolve, reject)=>{
-      storageService.deleteComment(id).
-        then(deletedComment=>resolve(deletedComment)).
-        catch(error=>reject(error))
-      })
-    }
-  };
-
-  return oCommentService;
-
-})();
-
-const oCommentService = new commentService();
-
-module.exports = oCommentService;
+module.exports = commentService;
