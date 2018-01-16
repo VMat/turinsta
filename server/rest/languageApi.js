85e64530773963988ebb6f5fdc021a5a34cf7bcb
Matías <Villalvama@gmail.com>
2018-01-16 15:49:01 -0300

    Update languageApi.js

--- server/rest/languageApi.js
+++ server/rest/languageApi.js
@@ -1,1 +1,16 @@
+const router = require('express').Router();
+const languageService = require('../services/languageService');
+  
+router.get('/',(req, res)=>{
+  languageService.getLanguages()
+    .then(languages=>{res.status(200).json(languages)})
+    .catch(error=>{res.status(500).send(error)})
+});
 
+router.get('/:id',(req, res)=>{
+  languageService.getLanguage(req.params.id)
+    .then(language=>{res.status(200).json(language)})
+    .catch(error=>{res.status(500).send(error)})
+});
+
+module.exports = router;
