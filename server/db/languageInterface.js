257f97e220609c436b916b234aabfce10e2984b6
Matías <Villalvama@gmail.com>
2018-01-16 15:57:03 -0300

    Update languageInterface.js

--- server/db/languageInterface.js
+++ server/db/languageInterface.js
@@ -1,1 +1,14 @@
+const Languages = require('../models/language');
+const Commons = require('./commons');
 
+let LanguageInterface = {};
+
+LanguageInterface.getAll = ()=>{
+  return Commons.getAll(Languages);
+};
+
+LanguageInterface.getOne = (id)=>{
+  return Commons.getOne(Languages, id)
+};
+
+module.exports = LanguageInterface;
