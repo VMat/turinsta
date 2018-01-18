8dbcbbdef03f7081259296f399a3847ca46b3e69
Matías <Villalvama@gmail.com>
2018-01-18 13:14:50 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ app.use(cors());*/
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(cors());
 
 app.use((req, res, next)=>{
   res.header("Access-Control-Allow-Origin", "*");
