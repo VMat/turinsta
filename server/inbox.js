fd3fba6266f1d194dc19b1b3c218c82d2db4085a
Matías <Villalvama@gmail.com>
2018-01-25 17:26:37 -0300

    Update inbox.js

--- server/inbox.js
+++ server/inbox.js
@@ -1,6 +1,7 @@
 let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+let InboxService = require('./services/inboxService');
  
 io.on('connection', (socket) => {
   
@@ -8,18 +9,20 @@ io.on('connection', (socket) => {
     io.emit('users-changed', {user: socket.nickname, event: 'left'});   
   });
  
-  socket.on('set-nickname', (nickname) => {
-    socket.nickname = nickname;
+  socket.on('set-nickname', (data) => {
+    socket.nickname = data.nickname;
+    socket.inbox = data.inbox;
     io.emit('users-changed', {user: nickname, event: 'joined'});    
   });
   
   socket.on('add-message', (message) => {
-    io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
+    io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});
+    InboxService.saveMessage(socket.inbox,{content: message.text, author: socket.nickname, timestamps: {created: new Date(), modified: null}});
   });
 });
  
 var port = 3001;
  
 http.listen(port, function(){
-   console.log('listening in http://localhost:' + port);
+   console.log('Socket.io is listening on port: ' + port);
 });
