66b750b4390b4d6504442fe645d3c07d4f01540a
Matías <Villalvama@gmail.com>
2018-01-26 14:30:39 -0300

    Update inboxServer.js

--- server/inboxServer.js
+++ server/inboxServer.js
@@ -8,19 +8,22 @@ inboxServer.init = (server)=>{
   
   io.on('connection', (socket) => {
 
+    socket.user = socket.handshake.query.user;
+    socket.inbox = socket.handshake.query.inbox;
+    
     socket.on('disconnect', function(){
-      io.emit('users-changed', {user: socket.nickname, event: 'left'});
+      io.emit('users-changed', {user: socket.user, event: 'left'});
     });
 
     socket.on('set-nickname', (data) => {
-      socket.nickname = data.nickname;
+      socket.user = data.user;
       socket.inbox = data.inbox;
-      io.emit('users-changed', {user: socket.nickname, event: 'joined'});
+      io.emit('users-changed', {user: socket.user, event: 'joined'});
     });
 
     socket.on('add-message', (message) => {
-      io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});
-      InboxService.saveMessage(socket.inbox,{content: message.text, author: socket.nickname, timestamps: {created: new Date(), modified: null}});
+      io.emit('message', {text: message.text, from: socket.user, created: new Date()});
+      InboxService.saveMessage(socket.inbox,{content: message.text, author: socket.user, timestamps: {created: new Date(), modified: null}});
     });
   });
 };
