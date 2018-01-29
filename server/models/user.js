3cff41da70d655ed97384d42cda5f60febb964dd
Matías <Villalvama@gmail.com>
2018-01-29 16:08:39 -0300

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -14,7 +14,8 @@ const userSchema = new schema({
 			type: schema.Types.ObjectId, ref: 'Activities'
 		}],
     unreadMessages:[{
-      type: schema.Types.ObjectId, ref: 'Inboxes'
+      inbox: {type: schema.Types.ObjectId, ref: 'Inboxes'},
+      messages: [] 
     }]
   },
   favorites: [{type: schema.Types.ObjectId, "ref": 'Publications'}],
