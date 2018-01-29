a26b2efeed00c41b6a6f575db5dcee3e92e283e5
Matías <Villalvama@gmail.com>
2018-01-29 17:30:13 -0300

    Update inboxInterface.js

--- server/db/inboxInterface.js
+++ server/db/inboxInterface.js
@@ -37,6 +37,8 @@ InboxInterface.saveMessage = (id,message)=>{
       return Commons.getOne(Inboxes,id)
         .then((inboxUpdated)=>{
           return Promise.all(inboxUpdated.participants.map((user)=>{
+            console.log("user: " + user);
+            console.log("author: " + inboxUpdated.messages[inboxUpdated.messages.length - 1].author);
             if(user !== inboxUpdated.messages[inboxUpdated.messages.length - 1].author){
               return UserInterface.addUnreadMessage(user,id,inboxUpdated.messages[inboxUpdated.messages.length - 1]); 
             }            
