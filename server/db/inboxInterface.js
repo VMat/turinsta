88ea1afcd47bde90c0fbe2bee0d63d9400b857e9
Matías <Villalvama@gmail.com>
2018-01-29 16:28:15 -0300

    Update inboxInterface.js

--- server/db/inboxInterface.js
+++ server/db/inboxInterface.js
@@ -30,13 +30,16 @@ InboxInterface.deleteOne = (id)=>{
 InboxInterface.saveMessage = (id,message)=>{
   return Commons.getOne(Inboxes,id)
     .then((inbox)=>{
-      return Promise.all(inbox.participants.map((user)=>{
-        return UserInterface.addUnreadMessage(user,id,message)  
-      }))
-      .then(()=>{
-        inbox.messages.push(message);
-        return Commons.update(Inboxes,inbox);
-      })
+      inbox.messages.push(message);
+      return Commons.update(Inboxes,inbox)
+    })
+    .then(()=>{
+      return Commons.getOne(Inboxes,id)
+        .then((inboxUpdated)=>{
+          return Promise.all(inboxUpdated.participants.map((user)=>{
+            return UserInterface.addUnreadMessage(user,id,inboxUpdated.messages[inboxUpdated.messages.length - 1]);  
+          }))         
+        })
     });
 };
 
