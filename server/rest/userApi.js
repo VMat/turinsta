ea8afccaede280b5693d4b00e60a8edd02b5c3f0
Matías <Villalvama@gmail.com>
2018-01-30 13:33:23 -0300

    Update userApi.js

--- server/rest/userApi.js
+++ server/rest/userApi.js
@@ -49,5 +49,12 @@ router.delete('/followers/:followed/:follower',(req, res)=>{
     .catch(error=>{res.status(500).send(error)})
 });
 
+router.delete('/notifications/user/:user/inbox/:inbox',(req, res)=>{
+  userService.removeUnreadMessages(req.params.user, req.params.inbox)
+    .then(user=>{res.status(200).json(user)})
+    .catch(error=>{res.status(500).send(error)})
+});
+ 
+
 
 module.exports = router;
