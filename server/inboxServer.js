06e80b2d5e01137727849812332f2f275b5bf583
Matías <Villalvama@gmail.com>
2018-02-09 13:45:21 -0300

    Update inboxServer.js

--- server/inboxServer.js
+++ server/inboxServer.js
@@ -32,17 +32,15 @@ inboxServer.init = (server)=>{
     });
 
     socket.on('message-read', (data)=>{
-      if(data.user == socket.user){
-        UserService.removeUnreadMessages(socket.user,socket.inbox)
-          .then((updatedUser)=>{
-              let inboxTarget = updatedUser.notifications.unreadMessages.filter((inbox)=>{return inbox.inbox.equals(socket.inbox)});
-              if(inboxTarget.length>0){
-                inboxTarget[0].messages.forEach((message)=>{
-                  io.in(socket.inbox).emit('read',{...data,message: message._id, status: {name:"READ",date: new Date().toISOString()}});
-                });
-              }
-          });
-      }
+      UserService.removeUnreadMessages(socket.user,socket.inbox)
+        .then((updatedUser)=>{
+            let inboxTarget = updatedUser.notifications.unreadMessages.filter((inbox)=>{return inbox.inbox.equals(socket.inbox)});
+            if(inboxTarget.length>0){
+              inboxTarget[0].messages.forEach((message)=>{
+                io.in(socket.inbox).emit('read',{...data,message: message._id, status: {name:"READ",date: new Date().toISOString()}});
+              });
+            }
+        });
     });
 
     socket.on('add-message', (message) => {
