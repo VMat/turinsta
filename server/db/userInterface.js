2cb490963c06ba3dad9b6add0c2f2b6aa21b750c
VMat <villalvama@gmail.com>
2018-03-01 15:30:13 -0300

    Implementing inbox writing

--- server/db/userInterface.js
+++ server/db/userInterface.js
@@ -21,14 +21,11 @@ UserInterface.getOne = (id,fields)=>{
 };
 
 UserInterface.getFollowedes = (id, n)=>{
-  let filters = Commons.processAggregateParams({"$user.followers": {operation: "CONTAINS", value: id}});
+  let filters = Commons.processAggregateParams({"_id": {operation: "EQUAL", value: id}});
   console.log("FILTERS: " + JSON.stringify(filters));
   return Users.aggregate([
-    {$group: {
-      user: {$first: "$$ROOT"}
-    }},
     ...filters,
-    {$sort: {"$user.username": 1}},
+    {$sort: {"username": 1}},
     {$limit: Number(n)}
   ]).exec();
 };
