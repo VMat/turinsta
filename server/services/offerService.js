d54ca02c6565fcb46e4bdacefdf518e4c6316e8a
VMat <villalvama@gmail.com>
2018-08-06 21:09:45 -0300

    Get offers added

--- server/services/offerService.js
+++ server/services/offerService.js
@@ -5,9 +5,9 @@ let OfferService = {};
 OfferService.getOffers = () => {
   return new Promise((resolve, reject) => {
     return Axios.get('https://despegar.com')
-    .then((result) => {
+    .then((res) => {
       console.log('PAGE', result.data);
-      resolve(JSON.stringify(result));
+      resolve(JSON.stringify(result.data));
     })
     .catch((err) => {
       reject(err);
