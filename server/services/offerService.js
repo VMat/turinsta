c5a15d3910b0f5942c351e7ed39f5d05280266b0
VMat <villalvama@gmail.com>
2018-08-08 22:42:20 -0300

    htmltojson added

--- server/services/offerService.js
+++ server/services/offerService.js
@@ -18,7 +18,7 @@ OfferService.getOffers = () => {
   return new Promise((resolve, reject) => {
     HtmlToJson.request('https://despegar.com', {
       'offers': ['.ux-home-offer', function (offer) {
-        return offer;
+        return JSON.stringify(offer);
       }]
     }, function (err, result) {
       if(err) reject(err);
