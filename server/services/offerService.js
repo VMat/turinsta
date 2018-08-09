c882ee80e3f0573dd3ae4b1ec0073a74e6bb3004
VMat <villalvama@gmail.com>
2018-08-08 22:32:59 -0300

    htmltojson added

--- server/services/offerService.js
+++ server/services/offerService.js
@@ -1,16 +1,29 @@
 const Axios = require('axios');
+const HtmlToJson = require('html-to-json');
 
 let OfferService = {};
 
 OfferService.getOffers = () => {
+  // return new Promise((resolve, reject) => {
+  //   return Axios.get('https://despegar.com')
+  //   .then((res) => {
+  //     console.log('PAGE', res.data);
+  //     resolve(res.data);
+  //   })
+  //   .catch((err) => {
+  //     reject(err);
+  //   });
+  // });
+
   return new Promise((resolve, reject) => {
-    return Axios.get('https://despegar.com')
-    .then((res) => {
-      console.log('PAGE', res.data);
-      resolve(res.data);
-    })
-    .catch((err) => {
-      reject(err);
+    HtmlToJson.request('https://despegar.com', {
+      'offers': ['.ux-home-offer', function (offer) {
+        return offer;
+      }]
+    }, function (err, result) {
+      if(err) reject(err);
+      console.log('htmltojson', result);
+      resolve(result);
     });
   });
 };
