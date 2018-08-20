f25d56b38cd3176b0306e024489752ae4207c568
VMat <villalvama@gmail.com>
2018-08-20 19:14:21 -0300

    html2json added

--- server/services/offerService.js
+++ server/services/offerService.js
@@ -1,37 +1,39 @@
 const Axios = require('axios');
 const HtmlToJson = require('html-to-json');
+const html2json = require('html2json').html2json;
 
 let OfferService = {};
 
 OfferService.getOffers = () => {
-  // return new Promise((resolve, reject) => {
-  //   return Axios.get('https://despegar.com')
-  //   .then((res) => {
-  //     console.log('PAGE', res.data);
-  //     resolve(res.data);
-  //   })
-  //   .catch((err) => {
-  //     reject(err);
-  //   });
-  // });
-
   return new Promise((resolve, reject) => {
-    HtmlToJson.request('https://despegar.com',
-      ['.ux-home-offer', {
-        'description': function ($offer) {
-          return $offer.text();
-        },
-        'image': function ($offer) {
-          return $offer.find('img').attr('src');
-        }}
-      ],
-      function (err, result) {
-        if(err) reject(err);
-        console.log('htmltojson', result);
-        resolve(result);
-      }
-    );
+    return Axios.get('https://despegar.com')
+    .then((res) => {
+      const jsonCreated = html2json(res.data);
+      console.log('PAGE', jsonCreated);
+      resolve(jsonCreated);
+    })
+    .catch((err) => {
+      reject(err);
+    });
   });
+
+  // return new Promise((resolve, reject) => {
+  //   HtmlToJson.request('https://despegar.com',
+  //     ['.ux-home-offer', {
+  //       'description': function ($offer) {
+  //         return $offer.text();
+  //       },
+  //       'image': function ($offer) {
+  //         return $offer.find('img').attr('src');
+  //       }}
+  //     ],
+  //     function (err, result) {
+  //       if(err) reject(err);
+  //       console.log('htmltojson', result);
+  //       resolve(result);
+  //     }
+  //   );
+  // });
 };
 
 module.exports = OfferService;
