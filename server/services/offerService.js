899049082e4da32b51fa5c438b1cecb0afdd6d47
VMat <villalvama@gmail.com>
2018-08-20 20:36:02 -0300

    htmltojson added

--- server/services/offerService.js
+++ server/services/offerService.js
@@ -8,8 +8,8 @@ OfferService.getOffers = () => {
   return new Promise((resolve, reject) => {
     return Axios.get('https://despegar.com')
     .then((res) => {
-      console.log('PAGE', res.body);
-      const jsonCreated = html2json(res.body);
+      console.log('PAGE', res.text());
+      const jsonCreated = html2json(res.text());
       console.log('JSON', jsonCreated);
       resolve(jsonCreated);
     })
